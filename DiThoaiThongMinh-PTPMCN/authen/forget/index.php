d5420e4e61008b45454fed3bd539396625e39642
Trần Văn Tới <94825061+TranVanToipp@users.noreply.github.com>
2022-10-05 21:35:14 +0700

    kiểm tra input form forget

--- DiThoaiThongMinh-PTPMCN/authen/forget/index.php
+++ DiThoaiThongMinh-PTPMCN/authen/forget/index.php
@@ -11,31 +11,43 @@
 <body>
 <div class="main">
 
-<form action="" method="POST" class="form" id="form-1">
-  <h3 class="heading">Đổi mật khẩu</h3>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" class="form" id="form-1">
+    <h3 class="heading">Đổi mật khẩu</h3>
 
-  <div class="spacer"></div>
+    <div class="spacer"></div>
 
-  <div class="form-group">
-    <label for="fullname" class="form-label">Tên tài khoản: </label>
-    <input id="fullname" name="fullname" type="text" placeholder="VD: trantoi" class="form-control">
-    <span class="form-message"></span>
-  </div>
+    <div class="form-group">
+        <label for="username" class="form-label">Tên tài khoản: </label>
+        <input id="username" name="userName" required type="text" minlength="6" placeholder="VD: trantoi" class="form-control">
+        <span class="form-message"></span>
+    </div>
 
-  <div class="form-group">
-    <label for="password" class="form-label">Nhập mật khẩu cũ: </label>
-    <input id="password" name="password" type="password"  class="form-control">
-    <span class="form-message"></span>
-  </div>
-  <div class="form-group">
-    <label for="password" class="form-label">Nhập mật khẩu mới: </label>
-    <input id="password" name="password" type="password"  class="form-control">
-    <span class="form-message"></span>
-  </div>
-
-  <button class="form-submit">Đổi mật khẩu</button>
+    <div class="form-group">
+        <label for="password" class="form-label">Nhập mật khẩu cũ: </label>
+        <input id="password" name="password" required type="password" minlength="6" class="form-control">
+        <span class="form-message"></span>
+    </div>
+    <div class="form-group">
+        <label for="password" class="form-label">Nhập mật khẩu mới: </label>
+        <input id="password" name="password" required type="password" minlength="6" class="form-control">
+        <span class="form-message"></span>
+    </div>
+    <button class="form-submit">Đổi mật khẩu</button>
 </form>
-
+  <?php
+			if(isset($_POST['userName'])&&isset($_POST['password'])){
+				$url = "http://localhost/WebDiDong_PTPMCN/DiThoaiThongMinh-PTPMCN/PHPREST/api/user/read_user.php";
+				$json=file_get_contents($url);
+				$data = json_decode($json);
+				$username = $_POST['userName'];
+				$pass= md5($_POST['password']);
+				foreach($data->data as $user){
+					if($username == $user->userName && $pass == $user->password)
+						 header('Location:../../../../../../WebDiDong_PTPMCN/DiThoaiThongMinh-PTPMCN');
+				}
+				echo "tài khoản or mật khẩu không đúng";
+			}
+		?>
 </div>
 
 </body>
