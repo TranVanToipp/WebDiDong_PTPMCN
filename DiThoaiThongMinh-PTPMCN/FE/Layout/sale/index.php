b004fce46011b59e65061a3a4b6c01b47c876911
TranVanToipp <94825061+TranVanToipp@users.noreply.github.com>
2022-10-11 15:23:06 +0700

    tách sale ở header

---
+++ DiThoaiThongMinh-PTPMCN/FE/Layout/sale/index.php
@@ -0,0 +1,83 @@
+
+    <div class="grid wide">
+        <div class="header__advertise">
+            <img src="./assets/img/quangcao-2/banner-dai_1652116489.webp" alt="">
+        </div>
+        <!-- Slide show products-sale -->
+        <div class="product_box-slide ">
+            <div class="product_box-slide-heading">
+                <div class="product_box-slide-img">
+                    <img src="#" alt="" class="product_box-slide-img-left">
+                </div>
+                <h2 class="title_sale-product">
+                    săn sale giá sốc mỗi ngày 
+                </h2>
+                <div class="product_box-slide-img">
+                    <img src="#" alt="" class="product_box-slide-img-right">
+                </div>
+            </div>
+            <div class="box-sale-products-content">
+                <div class="box-sale-products-item">
+                <div class="col l-2 m-4 c-6">
+                    <div class="home-product-item">
+                        <div class="home-product-item-img" style="background-image:url(./assets/img/iphone22.jpg);"></div>
+                        <h4 class="home-product-item-name">Điện thoại thông minh sắp ra mắt</h4>
+                        <div class="home-product-item-price">
+                            <span class="home-product-item__price-current">12.000.000Đ</span>
+                            <span class="home-product-item__price-old">12.000.000Đ</span>
+                        </div>
+                        <div class="home-product-item__sale-off">
+                            <span class="home-product-item__sale-off-percent">20%</span>
+                        </div>
+                        <div class="sale-time">
+                            <div class="sale_time-down">
+                                <span class="icon-sale">
+                                    <img src="#" alt="">
+                                </span>
+                                <div class="sale_time-down-content">
+                                    <span class="sale_time-down-itemday">
+                                        <b>0d</b>
+                                    </span>
+                                    <span class="sale_time-down-itemhours">
+                                        <b>2h</b>
+                                    </span>
+                                    <span class="sale_time-down-itemminute">
+                                        <b>23m</b>
+                                    </span>
+                                    <span class="sale_time-down-itemsecond">
+                                        <b>29s</b>
+                                    </span>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-sale-next">
+                    <div class="box-sale-next-left">
+
+                    </div>
+                <div class="box-sale-next-right">
+                    
+                </div>
+            </div>
+                </div>
+            </div>
+            
+        </div>
+        
+        <!-- render Loại sản phẩm -->
+        
+        <!-- Render sản phẩm -->
+        
+        
+    </div>
+
+    <div class="grid wide">
+        <div class="product-laysanpham">
+            <?php
+                include_once './ProductSP/render.php';
+            ?>
+        </div>
+    </div>
+    <script src="./Javascript/chitietsanpham.js"></script>
