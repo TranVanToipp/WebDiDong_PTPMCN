37181541c18eccf7938f1e6ebd032b2579e7af9e
TranVanToipp <94825061+TranVanToipp@users.noreply.github.com>
2022-10-11 15:26:41 +0700

    api chi tiết  sản phẩm

--- DiThoaiThongMinh-PTPMCN/PHPREST/api/product/read_single.php
+++ DiThoaiThongMinh-PTPMCN/PHPREST/api/product/read_single.php
@@ -9,12 +9,27 @@ include_once('../../core/initialize.php');
 
 //khởi tạo product
 $product = new product($db);
-
+$product_img = new product($db);
 $product->id = isset($_GET['id']) ? $_GET['id'] : die();
+$product_img->id = isset($_GET['id']) ? $_GET['id'] : die();
 //product query
 $product->read_single();
+$result = $product_img->img_desct();
+$product_arr['data'] = array();
+$num = $result->rowCount();
+$img_arr['img'] = array();
+if($num > 0){
+	while($row = $result->fetch(PDO::FETCH_ASSOC)){
+		extract($row);
+		$img_item = array(
+			'product_id' =>$id,
+			'img_desct' => $img_desct
+		);
+		array_push($img_arr['img'],$img_item);
+	}
+}
 
-$product_arr = array(
+$product = array (
 	'id' 	=>$product->id,
 	'product_type_name' =>$product->product_type_name,
 	'title' =>$product->title,
@@ -24,7 +39,9 @@ $product_arr = array(
 	'description' =>html_entity_decode($product->description),//chuyển đổi các kí hiệu HTML entities thành các kí tự tương ứng
 	'description2' =>html_entity_decode($product->description2),
 	'created_at' =>$product->created_at,
+	'img' =>$img_arr['img'],
 );
+array_push($product_arr['data'],$product);
 	//chuyển đổi sang dạng JSON
 	echo json_encode($product_arr);
 ?>
