566b7bb9a66106a609c490f9d3a70598397fa4c3
TranVanToipp <94825061+TranVanToipp@users.noreply.github.com>
2022-10-12 15:32:22 +0700

    thêm thuộc tính select cart

--- DiThoaiThongMinh-PTPMCN/PHPREST/core/cart.php
+++ DiThoaiThongMinh-PTPMCN/PHPREST/core/cart.php
@@ -11,10 +11,38 @@
 		public $user_name;
 		public $product_id;
 		public $price;
-		public $num;
+		public $num; //so luong sp
+		public $title;
+		public $discount;//% giảm giá
+		public $thumnail;
 		//hàm tạo với kết nối db
 		public function __construct($db){
 			$this->comn=$db;
+		}
+		public function select(){
+			$query = 'SELECT c.id, 
+			c.user_id, 
+			c.user_name, 
+			c.product_id, 
+			c.price as discount, 
+			c.num,
+			p.title,
+			p.price,
+			p.thumnail
+			
+			FROM '.$this->table.' c 
+			LEFT JOIN 
+					product p ON c.product_id = p.id
+					WHERE c.user_id = ?';
+			
+
+			$stmt = $this->comn->prepare($query);
+			$stmt->bindParam(1,$this->user_id);
+			//execute query
+			$stmt->execute();
+			
+			return $stmt;
+
 		}
 		public function create(){
 			//create query
