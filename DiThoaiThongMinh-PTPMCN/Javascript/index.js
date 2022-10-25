a4235ef7e6f3989bd7282f754539960fdea16ec1
TranVanToipp <94825061+TranVanToipp@users.noreply.github.com>
2022-10-25 16:18:05 +0700

    update model chi tiết

--- DiThoaiThongMinh-PTPMCN/Javascript/index.js
+++ DiThoaiThongMinh-PTPMCN/Javascript/index.js
@@ -1,26 +1,29 @@
-const UserAPI = 'http://localhost/WebDiDong_PTPMCN/DiThoaiThongMinh-PTPMCN/PHPREST/api/user/user_json.json';
-function start() {
-    
-    getUser(handleLogin);
+// FILE NÀY SẼ VIẾT CÁC TÍNH NĂNG TRÊN TRANG 
+// đoạn này code scroll trang
+const $1 = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+window.onscroll = () => {
+    if (this.pageYOffset > 150){
+        $1('.header').classList.add('sticky');
+    }else {
+        $1('.header').classList.remove('sticky');
+    }
 }
 
-function getUser(callback) {
-    fetch(UserAPI)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(callback);
-}
-function handleLogin(data) {
-    var userName = document.getElementById('userName').value; 
-    var password = document.getElementById('password').value; 
 
-    data.data.forEach(function (user) {
-        if (user.userName ==userName && user.password == password) {
-            alert("dang nhap thanh cong");
-        }else {
-            alert('Đăng nhập không thành công!!!');
-        }
-    });
-
+const btnclodeModel = document.querySelector('.close-model-chitiet');
+const btnHienModel = document.querySelector('.tinhtrang-button__chitiet-link');
+const model = document.querySelector('.modal-chitiet');
+console.log(model,btnHienModel);
+function showModelChiTiet() {
+    event.preventDefault();
+    model.classList.add('open')
 }
+
+function closeModelChiTiet() {
+    model.classList.remove('open');
+}
+
+btnHienModel.addEventListener('click',showModelChiTiet);
+btnclodeModel.addEventListener('click',closeModelChiTiet);
