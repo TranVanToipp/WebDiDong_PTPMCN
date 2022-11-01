95e26f6de934ad532159035ee2769795663cc226
Trần Văn Tới <94825061+TranVanToipp@users.noreply.github.com>
2022-11-01 19:15:11 +0700

    fix slide

--- DiThoaiThongMinh-PTPMCN/Javascript/slide.js
+++ DiThoaiThongMinh-PTPMCN/Javascript/slide.js
@@ -1,22 +1,22 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);    
+const $$$$ = document.querySelector.bind(document);
+const $$$ = document.querySelectorAll.bind(document);    
 
-const tabItems = $$('.tab-item');///dots
-const tabpanes = $$('.header__slide-inner');//slide
+const tabItems = $$$('.tab-item');///dots
+const tabpanes = $$$('.header__slide-inner');//slide
 
 tabItems.forEach((tab, index) => {
     const pane = tabpanes[index];
     tab.onclick = function () {
-        $('.tab-item.active').classList.remove('active');
-        $('.header__slide-inner.active').classList.remove('active');
+        $$$$('.tab-item.active').classList.remove('active');
+        $$$$('.header__slide-inner.active').classList.remove('active');
 
         pane.classList.add('active');
         this.classList.add('active');
     }
-})
+});
 
-const backBtn = $('.slide-left')
-const nextBtn = $('.slide-right')
+const backBtn = $$$$('.slide-left')
+const nextBtn = $$$$('.slide-right')
 var slideNumber = 0
 var slideIndex = 0
 nextBtn.addEventListener('click', () => {
